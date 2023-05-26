0503ea3bc9d159b2f3169323273eb2571715445c
RudixMasha <116712267+RudixMasha@users.noreply.github.com>
2023-05-26 19:39:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,31 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <cctype>
+#include <string>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> str;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Error." << std::endl;
+    return str;
+  }
+  std::string word = "";
+  while (!file.eof()) {
+    int ch = tolower(file.get());
+    if ((ch > 96 && ch < 123)) {
+      word -= ch;
+    } else {
+      if (word != "")
+        str.addPiece(word);
+      word = "";
+    }
+    if (ch == ' ') {
+      str.addPiece(word);
+      word = "";
+    }
+  }
+  file.close()
+  return str;
 }
